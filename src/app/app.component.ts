c795c9788091b1301afa8c378dfc1672d862ef38
Mariano Wilson <mariano.wilson@bp-4.com>
2021-04-05 19:14:05 -0300

    Agrego clase con color rojo a textos de validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,17 +10,40 @@ export class AppComponent implements OnInit {
 
   public forma: FormGroup;
 
-  public constructor() {}
+  public constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.forma = new FormGroup({ 
-      'nombre': new FormControl(''),
-      'apellido': new FormControl(''),
-      'dni': new FormControl(''),
-      'sexo': new FormControl(''),
-      'email': new FormControl(''),
-      'terminos': new FormControl('')
+
+    // **************************** //
+    // ******* MANERA 1 *********** //
+    // **************************** //
+
+    // this.forma = new FormGroup({ 
+    //   'nombre': new FormControl(''),
+    //   'apellido': new FormControl(''),
+    //   'dni': new FormControl(''),
+    //   'sexo': new FormControl(''),
+    //   'email': new FormControl(''),
+    //   'terminos': new FormControl('')
+    // });
+
+    // **************************** //
+    // ******* MANERA 2 *********** //
+    // **************************** //
+
+    this.forma = this.fb.group({
+      'nombre': ['', Validators.required],
+      'apellido': ['', Validators.required],
+      'dni': ['', Validators.required],
+      'sexo': ['', Validators.required],
+      'email': ['', ],
+      'terminos': ['', Validators.required]
     });
+
+  }
+
+  public aceptar(): void {
+    console.log(this.forma);
   }
 
 }
