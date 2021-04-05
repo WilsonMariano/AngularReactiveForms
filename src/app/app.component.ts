0933b5ebac5118955bf0d473ea80424939bb0419
Mariano Wilson <mariano.wilson@bp-4.com>
2021-04-05 18:57:57 -0300

    Cambio errores de html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularForms';
+export class AppComponent implements OnInit {
+
+  public forma: FormGroup;
+
+  public constructor() {}
+
+  ngOnInit(): void {
+    this.forma = new FormGroup({ 
+      'nombre': new FormControl(''),
+      'apellido': new FormControl(''),
+      'dni': new FormControl(''),
+      'sexo': new FormControl(''),
+      'email': new FormControl(''),
+      'terminos': new FormControl('')
+    });
+  }
+
 }
